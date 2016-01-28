0cc5923d3f4117451ad93255f2f18cbac34c4133
Tavis Ormandy <taviso@cmpxchg8b.com>
2016-01-27 19:12:22 -0800

    first attempt

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # rbndr
 Simple DNS Rebinding Service
+
+This is a very simple, non-conforming, name server for testing software against
+DNS rebinding vulnerabilities. The server responds to queries by randomly
+selecting one of the addresses specified in the hostname and returning it with
+a very low ttl.
+
+https://en.wikipedia.org/wiki/DNS_rebinding
+
+You would use this if you have a service that uses "preflight" checks to verify
+security. For example, consider a (fictional) browser plugin that has an api
+like this:
+
+```
+AllowUntrustedAccess("foobar.com");
+SendArbitraryRequests("foobar.com");
+```
+
+And AllowUntrustedAccess() simply sends a preflight request to the host:
+
+```
+GET /disablesecurity HTTP/1.1
+```
+
+If the service returns 200, then the plugin allows the hostpage complete access
+to that domain. This might be a security vulnerability, because you can specify
+a rbndr hostname, and then the plugin will allow complete access to an
+arbitrary ip address (e.g. an internal service).
+
+The format for hostnames is simply
+
+```
+<ipv4 in base-16>.<ipv4 in base-16>.rbndr.us
+```
+
+But you can use this website to convert from dotted quads if you prefer:
+
+https://lock.cmpxchg8b.com/rebinder.html
+
+
+For example, to switch between `127.0.0.1` and `192.168.0.1` you would use:
+
+```
+7f000001.c0a80001.rbndr.us
+```
+
+Let's test it out:
+
+```
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 192.168.0.1
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 192.168.0.1
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 192.168.0.1
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 127.0.0.1
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 127.0.0.1
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 192.168.0.1
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 127.0.0.1
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 127.0.0.1
+$ host 7f000001.c0a80001.rbndr.us
+7f000001.c0a80001.rbndr.us has address 192.168.0.1
+
+```
+
+As you can see, the server randomly returns one of the addresses.
+
---
+++ rebinder.c
@@ -0,0 +1,242 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <netdb.h>
+#include <time.h>
+#include <pwd.h>
+#include <err.h>
+#include <arpa/nameser.h>
+#include <arpa/inet.h>
+
+// This is a very very simple non-conforming nameserver for DNS rebinding
+// attacks. Do not use this code for anything.
+//
+//lint -e754 local struct member not referenced
+//lint -e716 while (1)
+//lint -e801 goto is deprecated
+
+#define __packed __attribute__((packed))
+
+struct qname {
+   uint8_t len;
+   uint8_t label[8];
+} __packed;
+
+struct __packed root {
+   struct __packed {
+       uint8_t len;        // 5
+       uint8_t data[5];    // 'r' 'b' 'n' 'd' 'r'
+   } domain;
+   struct __packed {
+       uint8_t len;        // 2
+       uint8_t data[2];    // 'u' 's'
+   } tld;
+   uint8_t root;           // 0
+};
+
+static const struct root kExpectedDomain = {
+   .domain = { 5, { 'r', 'b', 'n', 'd', 'r' } },
+   .tld    = { 2, { 'u', 's' } },
+   .root   = 0,
+};
+
+struct __packed header {
+   uint16_t id;
+   struct __packed {
+       unsigned  rd      : 1;
+       unsigned  tc      : 1;
+       unsigned  aa      : 1;
+       unsigned  opcode  : 4;
+       unsigned  qr      : 1;
+       unsigned  rcode   : 4;
+       unsigned  ra      : 1;
+       unsigned  ad      : 1;
+       unsigned  z       : 2;
+   } flags;
+   uint16_t qdcount;
+   uint16_t ancount;
+   uint16_t nscount;
+   uint16_t arcount;
+   struct __packed {
+       struct qname primary;
+       struct qname secondary;
+       struct root  domain;
+   } labels;
+   uint16_t qtype;
+   uint16_t qclass;
+   struct __packed {
+       uint8_t flag;
+       uint8_t offset;
+   } ptr;
+   uint16_t type;
+   uint16_t class;
+   uint32_t ttl;
+   uint16_t rdlength;
+   struct in_addr rdata;
+} __packed;
+
+//lint -efunc(713, parse_ip4_label) Loss of precision (initialization) (unsigned char to char)
+bool parse_ip4_label(struct in_addr *out, const uint8_t label[8])
+{
+    char ip4addr[] = {
+        '0', 'x', label[0], label[1],
+        label[2], label[3], label[4],
+        label[5], label[6], label[7],
+        0,
+    };
+
+    // Check for invalid characters
+    if (strspn(ip4addr + 2, "0123456789abcdefABCDEF") != strlen(ip4addr))
+        return false;
+
+    return inet_aton(ip4addr, out) != 0;
+}
+
+int main(int argc, char **argv)
+{
+    struct servent *domain;
+    struct passwd *nobody;
+    struct sockaddr_in server;
+    struct sockaddr_in address;
+    struct header reply;
+    struct header query;
+    socklen_t addrlen;
+    time_t querytime;
+    int sockfd;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        err(EXIT_FAILURE, "failed to create socket");
+    }
+
+    if ((domain = getservbyname("domain", "udp")) == NULL) {
+        errx(EXIT_FAILURE, "unable to lookup domain properties");
+    }
+
+    server.sin_family       = AF_INET;
+    server.sin_addr.s_addr  = INADDR_ANY;
+    server.sin_port         = domain->s_port; //lint !e734
+    addrlen                 = sizeof(address);
+    nobody                  = getpwnam("nobody");
+
+    if (nobody == NULL) {
+        errx(EXIT_FAILURE, "unable to lookup unprivileged user");
+    }
+
+    // Start listening for queries.
+    if (bind(sockfd, (struct sockaddr *) &server, sizeof(server)) != 0) {
+        errx(EXIT_FAILURE, "unable to bind server");
+    }
+
+    // Privileges no longer needed, so change to a chroot directory and setuid.
+    if (chdir("/var/empty") != 0 || chroot(".") != 0) {
+        errx(EXIT_FAILURE, "unable to change root directory");
+    }
+
+    // Change user.
+    if (setgid(nobody->pw_gid) != 0 || setuid(nobody->pw_uid) != 0) {
+        errx(EXIT_FAILURE, "unable to change to unprivileged user");
+    }
+
+    while (true) {
+        char clientaddr[INET_ADDRSTRLEN];
+
+        memset(&query, 0, sizeof query);
+        memset(&reply, 0, sizeof reply);
+
+        // Attempt to read a DNS query.
+        if (recvfrom(sockfd, &query, sizeof query, 0, (struct sockaddr *) &address, &addrlen) < 0) {
+            warn("error receiving query packet from network");
+            continue;
+        }
+
+        // Record time of request.
+        time(&querytime); //lint !e534
+
+        // Log query.
+        fprintf(stdout, "%s\t%s", inet_ntop(AF_INET, &address.sin_addr, clientaddr, sizeof(clientaddr)), ctime(&querytime));
+
+        // Duplicate the question into answer.
+        memcpy(&reply.labels, &query.labels, sizeof reply.labels);
+
+        reply.id            = query.id;
+        reply.flags.qr      = true;
+        reply.flags.aa      = true;
+        reply.ptr.flag      = NS_CMPRSFLGS;
+        reply.ptr.offset    = offsetof(struct header, labels);  //lint !e507
+        reply.type          = htons(ns_t_a);                    //lint !e641
+        reply.class         = htons(ns_c_in);                   //lint !e641
+        reply.ttl           = htonl(1);
+        reply.rdlength      = htons(sizeof reply.rdata);
+        reply.qtype         = query.qtype;
+        reply.qclass        = query.qclass;
+        reply.qdcount       = query.qdcount;
+        reply.ancount       = query.qdcount;
+
+
+        // Some quick validation.
+        if (query.qdcount != htons(1)) {
+            warnx("more than one question per query is not supported (%u queries)", ntohs(query.qdcount));
+            reply.flags.rcode = ns_r_notimpl; //lint !e641
+            goto error;
+        }
+
+        if (query.qtype != htons(ns_t_a)) { //lint !e641
+            warnx("unsupported qtype in question, returning no answers (qtype %u)", ntohs(query.qtype));
+            goto error;
+        }
+
+        if (query.labels.primary.len != 8) {
+            warnx("query with %u byte primary label (must be 8)", query.labels.primary.len);
+            reply.flags.rcode = ns_r_nxdomain; //lint !e641
+            goto error;
+        }
+
+        if (query.labels.secondary.len != 8) {
+            warnx("query with %u byte secondary label (must be 8)", query.labels.secondary.len);
+            reply.flags.rcode = ns_r_nxdomain; //lint !e641
+            goto error;
+        }
+
+        if (memcmp(&query.labels.domain, &kExpectedDomain, sizeof kExpectedDomain) != 0) {
+            warnx("query for unrecognised domain (must be .rbndr.us)");
+            reply.flags.rcode = ns_r_nxdomain; //lint !e641
+            goto error;
+        }
+
+        // Choose a random label to return based on ID.
+        if (!parse_ip4_label(&reply.rdata, (query.id & 1) ? query.labels.primary.label : query.labels.secondary.label)) {
+            warnx("client provided an invalid ip4 address, ignoring reqest");
+            reply.flags.rcode = ns_r_nxdomain; //lint !e641
+            goto error;
+        }
+
+        // Send response.
+        if (sendto(sockfd, &reply, sizeof reply, 0, (struct sockaddr *) &address, addrlen) != sizeof(reply)) { //lint !e737
+            warn("sendto failed to send response to query");
+        }
+
+        continue;
+
+  error:
+        reply.ancount = 0;
+
+        // Send an empty response (stop after question)
+        if (sendto(sockfd,
+                   &reply,
+                   offsetof(struct header, ptr),                //lint !e507
+                   0,
+                   (struct sockaddr *) &address,
+                   addrlen) != offsetof(struct header, ptr)) {  //lint !e737 !e507
+            warn("sendto failed to sending error response to unsupported query");
+            continue;
+        }
+    }
+
+    //lint -unreachable
+    return 0;
+}
